9a3aeb7396e8e7076ba4e34f782e8d24226f0aa4
billyjp1 <billyjp@appstate.edu>
2022-02-17 15:51:10 -0500

    Working on file upload button

--- Assets/Scripts/FileUpload.cs
+++ Assets/Scripts/FileUpload.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEditor;
 using System;
 using System.IO;
 
 public class FileUpload : MonoBehaviour
 {
-    public static void openExplorer() {
+    private GameObject button;
+    private Sprite selectedBoard;
+    public String buttonName;
+
+    void Start() {
+        button = GameObject.Find("Canvas").transform.Find(buttonName).gameObject;
+    }
+
+    public void openExplorer() {
         //Open file explorer and look for jpg/png file types
         string filePath = EditorUtility.OpenFilePanel("Game Board", "", "jpg,png");
         //If file path exists
@@ -17,7 +26,16 @@ public class FileUpload : MonoBehaviour
             string fname = splitF[splitF.Length-1];
             //Copy the image into the Boards folder with its old name
             System.IO.File.Copy(filePath, Path.Combine("Assets/Boards/", fname));
-        }
-    }   
 
+            selectBoard(fname);
+            
+        }
+    }
+
+
+    public void selectBoard(String name) {
+        //Set the selected board to the new file
+        selectedBoard = Resources.Load(Path.Combine("Assets/Boards/", name)) as Sprite;
+        button.GetComponent<Image>().sprite = selectedBoard;
+    }
 }
